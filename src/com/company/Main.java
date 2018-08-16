8c1b692f372faedef368f8b238f00f1bcd30406c
Kendra <kendra.mains10@gmail.com>
2018-08-16 17:11:18 -0500

    updated calculator with more operands

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,17 +8,79 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         Scanner kb = new Scanner(System.in);
-        System.out.println("/********************//");
-        System.out.println("//*** Gimme sum numbers ***//");
-        System.out.println("//**************************//");
-        int a = kb.nextInt();
-        System.out.println("and: ");
-        int b = kb.nextInt();
-        System.out.println(a +" + "+b+" = "+add(a, b));
+        char op = ',';
+
+        System.out.println("Welcome to a super basic calculator. \n" +
+                "Would you like to 1. Add \n" +
+                "2. Subtract \n" +
+                "3. Multiply \n" +
+                "4. Divide");
+        int choice = kb.nextInt();
+        switch(choice){
+            case 1:
+                op = '+';
+                break;
+            case 2:
+                op = '-';
+                break;
+            case 3:
+                op = '*';
+                break;
+            case 4:
+                op = '/';
+                break;
+            default:
+                op = '+';
+                break;
+        }
+        System.out.println("First Number: ");
+        int x = kb.nextInt();
+
+        System.out.println("Second Number: ");
+        int y= kb.nextInt();
+
+        System.out.println(x +" "+op+" "+y+" = "+ eval(choice, x, y));
+    }
+
+    public static double eval(int x, int a, int b){
+        double ans;
+        switch(x){
+            case 1:
+               ans = add(a,b);
+                break;
+            case 2:
+                ans = sub(a,b);
+                break;
+            case 3:
+                ans = mult(a,b);
+                break;
+            case 4:
+                ans = div(a,b);
+                break;
+            default:
+                ans = add(a,b);
+        }
+
+        return ans;
     }
 
     public static int add(int a, int b){
         int c = a+b;
+
+        return c;
+    }
+    public static int sub(int a, int b){
+        int c = a-b;
+
+        return c;
+    }
+    public static int mult(int a, int b){
+        int c = a*b;
+
+        return c;
+    }
+    public static double div(int a, int b){
+        double c = a/b;
 
         return c;
     }
