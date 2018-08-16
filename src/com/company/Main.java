ceaf79749c7933e7a5cd78e2c70afcdd83305afb
Kendra <kendra.mains10@gmail.com>
2018-08-16 15:44:18 -0500

    added adding to calc

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,25 @@
 package com.company;
 
+import java.util.*;
+import java.lang.*;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hello World");
+        Scanner kb = new Scanner(System.in);
+        System.out.println("/********************//");
+        System.out.println("//*** Gimme sum numbers ***//");
+        System.out.println("//**************************//");
+        int a = kb.nextInt();
+        System.out.println("and: ");
+        int b = kb.nextInt();
+        System.out.println(a +" + "+b+" = "+add(a, b));
+    }
+
+    public static int add(int a, int b){
+        int c = a+b;
+
+        return c;
     }
 }
